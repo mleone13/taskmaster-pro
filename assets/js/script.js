3b091b647a68c2a51f3af32a7d9010dfda95aeeb
Shooter McGavin <maryleone@rocketcentral.com>
2022-05-09 16:52:19 -0400

    drag-and-drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,57 +27,125 @@ var loadTasks = function () {
       done: [],
     };
   }
-
-  //due date was clicked
-  $(".list-group").on("click", "span", function () {
-    //get current text
-    var date = $(this).text().trim();
-
-    //create new input element
-    var dateInput = $("<input>")
-      .attr("type", "text")
-      .addClass("form-control")
-      .val(date);
-
-    //swap out elements
-    $(this).replaceWith(dateInput);
-
-    //automatically focus on new element
-    dateInput.trigger("focus");
-  });
-
-  //value of due date was changed
-  $(".list-group").on("blur", "input[type='text']", function () {
-    //get current text
-    var date = $(this).val().trim();
-
-    //get the parent ul's id attribute
-    var status = $(this).closest(".list-group").attr("id").replace("list-", "");
-
-    //get the tasks position in the list of the other li elements
-    var index = $(this).closest(".list-group-item").index();
-
-    //update the task in array and re-save to localstorage
-    tasks[status][index].date = date;
-    saveTasks();
-
-    //recreate span element with bootstrap classes
-    var taskSpan = $("<span>")
-      .addClass("badge badge-primary badge-pill")
-      .text(date);
-
-    //replace input with span element
-    $(this).replaceWith(taskSpan);
-  });
-
   // loop over object properties
   $.each(tasks, function (list, arr) {
+    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function (task) {
       createTask(task.text, task.date, list);
     });
   });
 };
+
+var saveTasks = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event, ui) {
+    console.log(ui);
+  },
+  deactivate: function (event, ui) {
+    console.log(ui);
+  },
+  over: function (event) {
+    console.log(event);
+  },
+  out: function (event) {
+    console.log(event);
+  },
+  update: function () {
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this);
+    $.children().each(function () {
+      tempArr.push({
+        text: $(this).find("p").text().trim(),
+        date: $(this).find("span").text().trim(),
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this).attr("id").replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  },
+  stop: function (event) {
+    $(this).removeClass("dropover");
+  },
+});
+
+// trash icon can be dropped onto
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    // remove dragged element from the dom
+    ui.draggable.remove();
+  },
+  over: function (event, ui) {
+    console.log(ui);
+  },
+  out: function (event, ui) {
+    console.log(ui);
+  },
+});
+
+//due date was clicked
+$(".list-group").on("click", "span", function () {
+  //get current text
+  var date = $(this).text().trim();
+
+  //create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  //swap out elements
+  $(this).replaceWith(dateInput);
+
+  //automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+//value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function () {
+  //get current text
+  var date = $(this).val().trim();
+
+  //get the parent ul's id attribute
+  var status = $(this).closest(".list-group").attr("id").replace("list-", "");
+
+  //get the tasks position in the list of the other li elements
+  var index = $(this).closest(".list-group-item").index();
+
+  //update the task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  //recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  //replace input with span element
+  $(this).replaceWith(taskSpan);
+});
+
+// loop over object properties
+$.each(tasks, function (list, arr) {
+  // then loop over sub-array
+  arr.forEach(function (task) {
+    createTask(task.text, task.date, list);
+  });
+});
 
 var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
